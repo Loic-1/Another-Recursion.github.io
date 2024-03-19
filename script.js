fb3ab05dbef05451dd337a1acd7180a8565bbd9e
Loic <loic.vasile@gmail.com>
2024-03-19 13:13:31 +0100

    presque fini

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// Get a reference to the button element
-var button = document.getElementById('hihi');
-
-var text = document.getElementById('text');
-
-var isVisible = true;
-
-// Add a click event listener to the button
-button.addEventListener('click', function() {
-    if (isVisible) {
-        text.style.opacity = '0';
-        isVisible = false;
-    } else {
-        text.style.opacity = '1';
-        isVisible = true;
-    }
-});
-
-
-
 var toggleButton = document.getElementById('toggleButton');
 
 var sidebar = document.getElementById('sidebar');
@@ -26,7 +6,92 @@ toggleButton.addEventListener('click', function() {
     sidebar.classList.toggle('collapsed');
 });
 
+//thomasP
 
-/* gsap.to(".box", {
-     x: 200,
- });*/
+var buttonThomas = document.getElementById('buttonThomas');
+
+var textThomas = document.getElementById('textThomas');
+
+var isVisible = true;
+
+buttonThomas.addEventListener('click', function() {
+    if (isVisible) {
+        textThomas.style.opacity = '0';
+        isVisible = false;
+    } else {
+        textThomas.style.opacity = '1';
+        isVisible = true;
+    }
+});
+
+//thomasH
+
+var buttonThom = document.getElementById('buttonThom');
+
+var textThom = document.getElementById('textThom');
+
+var isVisible2 = true;
+
+buttonThom.addEventListener('click', function() {
+    if (isVisible2) {
+        textThom.style.opacity = '0';
+        isVisible2 = false;
+    } else {
+        textThom.style.opacity = '1';
+        isVisible2 = true;
+    }
+});
+
+//Edgar
+
+var buttonEdgar = document.getElementById('buttonEdgar');
+
+var textEdgar = document.getElementById('textEdgar');
+
+var isVisible3 = true;
+
+buttonEdgar.addEventListener('click', function() {
+    if (isVisible3) {
+        textEdgar.style.opacity = '0';
+        isVisible3 = false;
+    } else {
+        textEdgar.style.opacity = '1';
+        isVisible3 = true;
+    }
+});
+
+//loic
+
+var buttonLoic = document.getElementById('buttonLoic');
+
+var textLoic = document.getElementById('textLoic');
+
+var isVisible4 = true;
+
+buttonLoic.addEventListener('click', function() {
+    if (isVisible4) {
+        textLoic.style.opacity = '0';
+        isVisible4 = false;
+    } else {
+        textLoic.style.opacity = '1';
+        isVisible4 = true;
+    }
+});
+
+//asmae
+
+var buttonAsmae = document.getElementById('buttonAsmae');
+
+var textAsmae = document.getElementById('textAsmae');
+
+var isVisible5 = true;
+
+buttonAsmae.addEventListener('click', function() {
+    if (isVisible5) {
+        textAsmae.style.opacity = '0';
+        isVisible5 = false;
+    } else {
+        textAsmae.style.opacity = '1';
+        isVisible5 = true;
+    }
+});
